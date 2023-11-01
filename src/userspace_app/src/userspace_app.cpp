68e281e35419a8394532724a3d4dd9d5c30ac1a0
bunszey <bunszey@gmail.com>
2023-11-01 15:16:23 +0100

    Added the ip function to userspace app

--- src/userspace_app/src/userspace_app.cpp
+++ src/userspace_app/src/userspace_app.cpp
@@ -6,6 +6,8 @@
 
 #include <chrono>
 
+#include "xexample.h"
+
 class ImageSubscriber : public rclcpp::Node
 {
 	public:
@@ -24,15 +26,32 @@ class ImageSubscriber : public rclcpp::Node
 					"/image_out",
 					10
 			);
-			timer_ = this->create_wall_timer(std::chrono::milliseconds(300), std::bind(&ImageSubscriber::timer_callback, this));
+			timer_ = this->create_wall_timer(std::chrono::milliseconds(100), std::bind(&ImageSubscriber::timer_callback, this));
+
+			char instance_name[ 20 ];
+			sprintf(instance_name, "example");
+
+			int status = XExample_Initialize(&ip_inst, instance_name);
+			if (status != XST_SUCCESS) {
+				printf("Error: Could not initialize the IP core.\n");
+				return XST_FAILURE;
+			}
 
 
+		}
+
+		~ImageSubscriber(){
+			// Cleanup
+			XExample_DisableAutoRestart(&ip_inst);
+			XExample_Release(&ip_inst);
+			cleanup_platform();
 		}
 
 	private:
 		rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr camera_subscription_;
 		rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr camera_publisher_;
 		rclcpp::TimerBase::SharedPtr timer_;
+		XExample ip_inst;
 
 		cv::Mat gray_;
 
@@ -47,12 +66,26 @@ class ImageSubscriber : public rclcpp::Node
 			cv::split(img, channels);
 
 			gray_ = channels[0];
-	
+
 			RCLCPP_INFO(this->get_logger(), "Successfully loaded image");
 		}
 
 		void timer_callback(){
-			sensor_msgs::msg::Image::SharedPtr msg = cv_bridge::CvImage(std_msgs::msg::Header(), "mono8", gray_).toImageMsg();
+			std::vector<unsigned char> vec_gray(gray_.begin<unsigned char>(), gray_.end<unsigned char>());
+			
+			XExample_Write_in_r_Bytes(&ip_inst, 0, vec_gray, DATA_SIZE);
+
+			// Call the IP core function
+			XExample_Start(&ip_inst);
+
+			// Wait for the IP core to finish
+			while (!XExample_IsDone(&ip_inst));
+
+			std::vector<unsigned char> out;
+			XExample_Read_out_r_Bytes(&ip_inst, 0, out, DATA_SIZE);
+			cv::Mat res = cv::Mat(out).reshape(0, gray_.rows);
+
+			sensor_msgs::msg::Image::SharedPtr msg = cv_bridge::CvImage(std_msgs::msg::Header(), "mono8", res).toImageMsg();
 			camera_publisher_->publish(*msg.get());
         	std::cout << "Published!" << std::endl;
 		}
